9183964e2c023ae6512c925d5ec43fae90d29f9f
MK <akhil.kondu@gmail.com>
2026-02-23 21:01:36 +0530

    index.js changed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Harness CI/CD Demo Running Successfully!");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Harness CI/CD Demo Running Successfully!");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
